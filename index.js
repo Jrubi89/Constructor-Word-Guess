2cfb9563c5ecf164e2dd823d244aaa0142662f15
unknown <jrubi89@icloud.com>
2020-06-17 19:37:05 -0400

    letters entry

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //Creating require connection//
 var word = require("./Word.js");
 var inquirer = require("inquirer");
+//Creating Logic //
+//Array for letters//
+var letterArray = "abcdefghijklmnopqrstuvwxyz";
+
