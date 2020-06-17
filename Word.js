e98f89c2910bee85be9819270496bab198a28ecc
unknown <jrubi89@icloud.com>
2020-06-17 19:19:11 -0400

    log function to display data in the terminal

--- Word.js
+++ Word.js
@@ -9,5 +9,20 @@ for(var i = 0; i < answer.length; i++){
     var letter = new Letter(answer[i]);
     this.objArray.push(letter);
     }
+//creating log function to display data in the terminal//
+this.log = function () {
+    answerLog = "";
+    for (var i = 0; i < this.objArray.length; i++) {
+        answerLog += this.objArray[i] + " ";
+    }
+    console.log(answerLog + "\n============================\n");
+};
+
+this.userGuess = function (input) {
+    for (var i = 0; i < this.objArray.length; i++) {
+        this.objArray[i].guess(input);
+    }
+  }; 
 }
 
+module.exports = Word; 
